4da87e7968a57639e2830495a612c84bd867e019
Ronen Shimon <48548239+ronen232@users.noreply.github.com>
2022-02-14 16:55:24 +0200

    Update getUser.py

--- getUser.py
+++ getUser.py
@@ -1,16 +1,21 @@
 
 import os
-
+import getpass
 # for k, v in os.environ.items():
 #     print(f'{k}={v}')
 
+import getpass
+user = getpass.getuser()
+print(user)
+
+
 # Get the list of user's
 # environment variables
-env_var = os.environ['USER']
+# env_var = os.environ['USER']
 # home_dir = os.environ['HOME']
 # username = os.environ['USER']
 # print(f'The user is {username} home directory is {home_dir}')
 # print("User's Environment variable:")
-print(env_var)
+# print(env_var)
 
 
