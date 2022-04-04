6580c370ef806f14a17c222381ac321ed8f70d0b
Ronen Shimon <48548239+ronen232@users.noreply.github.com>
2022-04-04 21:07:25 +0300

    Ronen shimon Test
    Test

---
+++ test/Count.py
@@ -0,0 +1,7 @@
+# 10 times
+# the last num is 10  when count will be equal 11 or greater will get out from the while loop
+count = 1
+while count < 11:
+    print(count)
+    count += 1
+print("\n")
